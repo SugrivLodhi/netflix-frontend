0569b4c9eeeeb308ebc28fbf63b78be2e90de139
SugrivLodhi <sugrivmlvt@gmail.com>
2024-04-21 23:57:19 +0530

    integrate signup api

--- src/component/AuthPage.js
+++ src/component/AuthPage.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./common/Input";
-
+import axios from 'axios'
+import { BASE_URL } from "../constant";
 const AuthPage = () => {
+ const [fullName,setFullName] = useState("")
+ const [email,setEmail] = useState("")
+ const [password,setPassword] = useState("")
+
+  const handleSignup = async(body)=>{
+      try{
+        await axios.post(`${BASE_URL}/register`,body)
+      }
+      catch(err){
+        console.log("first error",err)
+      } 
+  }
    const onSubmit = (e)=>{
     e.preventDefault()
+    const body ={
+       fullName,
+       email,
+       password
+    }
+   handleSignup(body)
+
   }
   return (
     <div className="flex items-center justify-center rounded  m-auto max-w-[450px] bg-black opacity-75">
       <div className="max-w-md w-full  p-8 rounded shadow-lg m-4 flex flex-col">
         <h2 className="text-3xl mb-5  font-semibold text-white">Sign Up</h2>
         <form onSubmit={onSubmit} className="flex flex-col gap-4">
-          <Input placeholder="Enter Full Name" />
-          <Input type="email" placeholder="Enter Email" />
-          <Input type="tel" placeholder="Enter Phone Number" />
+          <Input placeholder="Enter Full Name" onChange={(e)=>setFullName(e.target.value)} />
+          <Input type="email" placeholder="Enter Email" onChange={(e)=> setEmail(e.target.value)} />
+          <Input type="password" placeholder="Enter the password" onChange={(e)=>setPassword(e.target.value) } />
           <button
             type="submit"
             className="bg-red-700 text-white py-4 text-lg rounded transition duration-300"
--- src/component/common/Input.jsx
+++ src/component/common/Input.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Input = ({type="text",placeholder}) => {
+const Input = ({type="text",placeholder,onChange}) => {
   return (
     <input
      className='w-full px-2 py-4 outline-none border bg-black  focus:ring-white focus:ring-2 placeholder:text-lg text-lg border-gray-500 placeholder:text-gray-400 rounded-md '
      type={type}
      placeholder={placeholder}
+     onChange={(e)=>onChange(e)}
     />
   )
 }
